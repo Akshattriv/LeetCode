93d3e3406cdb8ab06c803df895f6f7ef9dac01b2
Akshat Trivedi <72058144+Akshattriv@users.noreply.github.com>
2022-03-09 12:36:02 +0530

    Create 204.Count_Primes.py

---
+++ 204.Count_Primes.py
@@ -0,0 +1,15 @@
+from math import isqrt
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n<=2:
+            return 0
+        is_prime = [True]*n
+        is_prime[0] = False
+        is_prime[1] = False
+        
+        for i in range(2, isqrt(n)+1):
+            if is_prime[i]:
+                for x in range(i*i, n, i):
+                    is_prime[x] = False
+                    
+        return len([i for i in range(n) if is_prime[i]])
