e659786c3cb6d298f508645871b143f6734e0d0a
Akshat Trivedi <72058144+Akshattriv@users.noreply.github.com>
2022-03-25 14:49:03 +0530

    Create 3_Plus_One.cpp

---
+++ 75_Day_Challenge_TechMaestro/Day 1 Arrays/3_Plus_One.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i = digits.size();
+        while(i--){
+            if(digits[i] != 9){
+                digits[i] += 1;
+                return digits;
+            }else
+                digits[i] = 0;      
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
