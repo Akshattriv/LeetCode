87f0f2496d7c9c4e0b180af039c72a9bb890f557
Akshat Trivedi <72058144+Akshattriv@users.noreply.github.com>
2022-03-26 20:24:05 +0530

    Create 3_Merge_Intervals.cpp

---
+++ 75_Day_Challenge_TechMaestro/Day 3/3_Merge_Intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>merged;
+        sort(intervals.begin(), intervals.end());
+        vector<int>temp = intervals[0];
+      
+        for(int i=1; i<intervals.size(); ++i){
+            if(temp[0] <= intervals[i][1] && temp[1]>=intervals[i][0]){
+                temp[1] = max(intervals[i][1],temp[1]);
+            }else{
+                merged.push_back(temp);
+                temp = intervals[i];
+            }
+        }
+        merged.push_back(temp);
+        return merged;
+    }
+};
